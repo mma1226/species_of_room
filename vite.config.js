55813c72c9fcd56460c942cc54236b6e0b9d5cf9
Glitch (bevel-pumped-king) <none>
2023-10-26 01:50:34 +0000

    Import of juniorxsound/itp-visual-journalism-fall-2023

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build"
-  },
   server: {
-    host:"0.0.0.0",
-    port:3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
-  }
+    port: 3000,
+  },
+  plugins: [react()],
 });

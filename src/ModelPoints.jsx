5427019eecff82c264bb727b0d7c1b994d797e4c
Glitch (bevel-pumped-king) <none>
2023-12-11 22:58:07 +0000

    🌞😺 Checkpoint ./src/model.json:41447567/189 ./src/ModelPoints.jsx:468637/4035

--- src/ModelPoints.jsx
+++ src/ModelPoints.jsx
@@ -1,50 +1,52 @@
-import { useGLTF, Html } from "@react-three/drei";
+import { useGLTF, Html, Box } from "@react-three/drei";
 import { useState } from "react";
 
 export default function ModelPoints(props) {
+  const { name, src, text } = props;
 
-  const {name, src, text} = props;
-  
   const [isExpanded, setIsExpanded] = useState(false);
 
   const { scene, nodes } = useGLTF(src);
-  let model= Object.values(nodes)[1];
+  let model = Object.values(nodes)[1];
 
   // nodes.room.geometry
   // nodes.bed.geometry
   // nodes.other_object.geometry
 
   return (
-    <points
+    <Box
       onPointerEnter={() => {
         //if you want more than one thing to happen
         //wrap these functions with a pair of curly bracket
-        console.log('hovered')
+        console.log("hovered");
         setIsExpanded(true);
       }}
       onPointerLeave={() => {
         setIsExpanded(false);
       }}
-      scale={[5, 5, 5]}
-      rotation={[Math.PI / 2, 0, 0]}
-      geometry={model.geometry}
     >
-      {isExpanded && (
-        <Html>
-          <div
-            style={{
-              color: "white",
-              padding: "10px",
-              background: "grey",
-              borderRadius: "5px",
-            }}
-          >
-            {text}
-          </div>
-        </Html>
-      )}
+      <points
+        scale={[5, 5, 5]}
+        rotation={[Math.PI / 2, 0, 0]}
+        geometry={model.geometry}
+      >
+        {isExpanded && (
+          <Html>
+            <div
+              style={{
+                color: "white",
+                padding: "10px",
+                background: "grey",
+                borderRadius: "5px",
+              }}
+            >
+              {text}
+            </div>
+          </Html>
+        )}
 
-      <pointsMaterial size={0.01} map={model.material.map} />
-    </points>
+        <pointsMaterial size={0.01} map={model.material.map} />
+      </points>
+    </Box>
   );
 }

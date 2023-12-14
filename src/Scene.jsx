30a32237d71dc99c31d65b8469a794292fe884c8
Glitch (bevel-pumped-king) <none>
2023-12-14 22:01:32 +0000

    🙌🏛 Checkpoint ./src/ModelPoints.jsx:22400771/22 ./.glitch-assets:22400771/796 ./src/Scene.jsx:41447567/6916 ./src/model.json:22400771/202

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,13 +1,12 @@
 import { Suspense, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { BackSide } from "three";
-import { Environment, Float } from "@react-three/drei";
-import { Preload } from "@react-three/drei";
+import { Environment, Float, Preload, Sparkles } from "@react-three/drei";
 
 import AnimatedCamera from "./AnimatedCamera";
 
 import ModelPoints from "./ModelPoints";
-import FX from "./FX"
+import FX from "./FX";
 
 function Scene() {
   //where to store data
@@ -42,7 +41,7 @@ function Scene() {
   return (
     <div id="canvas_wrapper">
       <Canvas>
-        < FX/>
+        <FX />
         <Environment preset="night" />
 
         {/* Camera 🎥 */}
@@ -57,7 +56,6 @@ function Scene() {
 
         {/* We can create a background color as a child element of the canvas we just have to attach it */}
         <color args={["black"]} attach="background" />
-
 
         {/* Data points turned into geometry with it's own interaction 📌 */}
 
@@ -77,6 +75,12 @@ function Scene() {
                 floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
                 floatingRange={[-0.3, 0.3]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
               >
+                <Sparkles
+                  count={50}
+                  scale={10}
+                  size={0.5}
+                  speed={0.1}
+                />
                 <ModelPoints
                   key={item.name}
                   name={item.name}

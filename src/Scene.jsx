b1b1cdd0cf363e414aa0f9698e802a9ab5fb7b12
Glitch (bevel-pumped-king) <none>
2024-02-29 22:21:37 +0000

    👦🌎 Checkpoint ./src/model.json:22400771/20 ./src/Scene.jsx:22400771/20

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -93,7 +93,7 @@ function Scene() {
                   <Html>
                 {/* Adjust position based on your preference */}
                 <div style={{ position: 'absolute', 
-                              top: 10, 
+                              top: '20vh', 
                               left: '10%', 
                               transform: 'translateX(-76%)', 
                               color: 'Pink', 
